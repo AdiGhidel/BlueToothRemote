26e9f3c5bfcd4d7847e19f16ef39d85691f51a2f
Adrian Ghidel <aghidel@amazon.com>
2019-02-25 20:46:25 +0000

    Initial Commit

---
+++ BlueToothRemoteTests/BlueToothRemoteTests.swift
@@ -0,0 +1,34 @@
+//
+//  BlueToothRemoteTests.swift
+//  BlueToothRemoteTests
+//
+//  Created by Ghidel, Marius-adrian on 2/25/19.
+//  Copyright © 2019 Ghidel, Marius-adrian. All rights reserved.
+//
+
+import XCTest
+@testable import BlueToothRemote
+
+class BlueToothRemoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
