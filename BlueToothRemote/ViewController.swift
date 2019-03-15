59401acf7a11930f9418e9264c83ccb935b67f7f
Adrian Ghidel <aghidel@amazon.com>
2019-03-15 20:58:31 +0000

    working remote bt

--- BlueToothRemote/ViewController.swift
+++ BlueToothRemote/ViewController.swift
@@ -9,61 +9,33 @@
 import UIKit
 import CoreBluetooth
 
-
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralManagerDelegate, BluetoothSerialDelegate {
-    
-    func serialDidChangeState() {
-        <#code#>
-    }
-    
-    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
-        <#code#>
-    }
-    
-   
-    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        print("peripheral updated sth")
-    }
-    
-    let debug = true
-    var robotBT: CBPeripheral?
-    var CBManager : CBCentralManager?
-    var PFManager : CBPeripheralManager?
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    //Vars
+    var central: CBCentralManager?
+    var peripheralR: CBPeripheral?
+    var writeCharacteristic: CBCharacteristic?
+    var writeType: CBCharacteristicWriteType?
+    var connected = false;
+    var debug = true;
     
     /// UUID of the service to look for.
-//    var serviceUUID = CBUUID(string: "FFE0")
-//    /// UUID of the characteristic to look for.
-//    var characteristicUUID = CBUUID(string: "FFE1")
-//    /// Write characteristic
-//    var writeType: CBCharacteristicWriteType = .withoutResponse
-//    var characteristicBT: CBCharacteristic?
+    var serviceUUID = CBUUID(string: "FFE0")
+    
+    /// UUID of the characteristic to look for.
+    var characteristicUUID = CBUUID(string: "FFE1")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        CBManager = CBCentralManager(delegate: self, queue: nil)
-        serial.delegate = self
+        //1 create a central manager
+        central = CBCentralManager(delegate: self, queue: nil)
     }
-    //Bluetooth
-    //CBCentralManager code
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+    
 
-        if let p = peripheral.name {
-            if p == "SH-M08" {
-                robotBT = peripheral
-                print("periferal name \(peripheral.name ?? "")")
-                print("periferal uuid \(peripheral.identifier.uuidString)")
-                print("RSSI\(RSSI)")
-                print("advertisment data \(advertisementData)")
-                print("*****************")
-            }
-        }
-    
-    }
-    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == .poweredOn {
-            central.scanForPeripherals(withServices: nil, options: nil)
+            //2 if on scan for device
+            central.scanForPeripherals(withServices: [serviceUUID], options: nil)
         } else {
             let alert = UIAlertController(title: "Bluetooth isn't working", message: "Make sure your Bluetooth is on", preferredStyle: .alert)
             let action = UIAlertAction(title: "Ok", style: .default) { (action) in
@@ -72,58 +44,99 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralMa
             alert.addAction(action)
             present(alert, animated: true, completion: nil)
         }
+    }
+    //3 if discovered connect
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral.name!)
         
+        peripheralR = peripheral
+        peripheralR?.delegate = self
     }
-    
-    //Commands
-    @IBAction func forward(_ sender: Any) {
-        let data: Data = "AAA".data(using: .utf8)!
-//        let ch : CBCharacteristic
-//        ch.value = "OxFFE1".data(using: .utf8)!
-//        robotBT?.writeValue(data, ch, .withoutResponse)
-    }
-    @IBAction func stop(_ sender: Any) {
-    }
-    @IBAction func back(_ sender: Any) {
-    }
-    @IBAction func left90(_ sender: Any) {
-    }
-    @IBAction func right90(_ sender: Any) {
-    }
-    @IBAction func spin(_ sender: Any) {
-    }
-    @IBAction func speed(_ sender: Any) {
-    }
-    @IBOutlet var speedOutlet: UISlider!
-    
-    
+    //5 did connect
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         if debug {
             print("connected")
             print(peripheral)
-            
         }
-        peripheral.delegate = self as! CBPeripheralDelegate
+        connected = true
         peripheral.discoverServices([serviceUUID])
         connectedOutlet.alpha = 1
     }
+    //6 did discover service
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        for service in peripheral.services! {
+            peripheral.discoverCharacteristics([characteristicUUID], for: service)
+        }
+    }
+    //7 did discover characteristics
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        for characteristic in service.characteristics! {
+            print(characteristic)
+            // keep a reference to this characteristic so we can write to it
+            writeCharacteristic = characteristic
+
+            // find out writeType
+            writeType = characteristic.properties.contains(.write) ? .withResponse : .withoutResponse
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        let alert = UIAlertController(title: "Stella ran away", message: "Porky stole the bluetooth", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        connected = false
+        connectedOutlet.alpha = 0;
+    }
+    
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        
         let alert = UIAlertController(title: "Could not connect", message: "Stella should reset the device", preferredStyle: .alert)
         let action = UIAlertAction(title: "Ok", style: .default) { (action) in
             alert.dismiss(animated: true, completion: nil)
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        connected = false
     }
+
+    //Commands
+    @IBAction func forward(_ sender: Any) {
+        sendMessageToDevice("forward");
+    }
+    @IBAction func stop(_ sender: Any) {
+        sendMessageToDevice("stop");
+    }
+    @IBAction func back(_ sender: Any) {
+        sendMessageToDevice("back");
+    }
+    @IBAction func left90(_ sender: Any) {
+        sendMessageToDevice("left90");
+    }
+    @IBAction func right90(_ sender: Any) {
+        sendMessageToDevice("right90");
+    }
+
+    @IBAction func spin(_ sender: Any) {
+        sendMessageToDevice("spin");
+    }
+    
+    @IBAction func speed(_ sender: Any) {
+        sendMessageToDevice("\(speedOutlet!.value)");
+    }
+    @IBOutlet var speedOutlet: UISlider!
+    //4 manually connect
     @IBAction func connectToDevice(_ sender: Any) {
         if debug {
             print("press connect")
         }
-        if let robotBT = robotBT {
+        if let peripheralR = peripheralR {
             if debug {
-                print("if \(robotBT)")
+                print("if \(peripheralR)")
             }
-            CBManager?.connect(robotBT, options: nil)
+            print("trying to connect")
+            central?.connect(peripheralR)
         } else {
             let alert = UIAlertController(title: "Device not found", message: "Stella should turn on the device", preferredStyle: .alert)
             let action = UIAlertAction(title: "Ok", style: .default) { (action) in
@@ -133,30 +146,14 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralMa
             present(alert, animated: true, completion: nil)
         }
     }
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        // discover the 0xFFE1 characteristic for all services (though there should only be one)
-        print("discovering services")
-        for service in peripheral.services! {
-            peripheral.discoverCharacteristics([characteristicUUID], for: service)
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        // check whether the characteristic we're looking for (0xFFE1) is present - just to be sure
-        for characteristic in service.characteristics! {
-            if characteristic.uuid == characteristicUUID {
-                // subscribe to this value (so we'll get notified when there is serial data for us..)
-                peripheral.setNotifyValue(true, for: characteristic)
-                
-                // keep a reference to this characteristic so we can write to it
-                characteristicBT = characteristic
-                
-                // find out writeType
-                writeType = characteristic.properties.contains(.write) ? .withResponse : .withoutResponse
-                print("ch: \(characteristic) wt: \(writeType)")
+    @IBOutlet var connectedOutlet: UIButton!
+    //Aux func
+    func sendMessageToDevice(_ message: String) {
+        if connected == true {
+            if let data = message.data(using: String.Encoding.utf8) {
+                print(data)
+                peripheralR!.writeValue(data, for: writeCharacteristic!, type: writeType!)
             }
         }
     }
-    @IBOutlet var connectedOutlet: UIButton!
 }
-
